7770469b0b5903dd44d6d42d2e1939ace5d956df
Pinkernell <LPinkernell@tobit.ag>
2020-08-21 18:11:46 +0200

    Create List with data from Chayns Json File

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-param-reassign */
 import './app.scss';
+import 'whatwg-fetch';
 
 
 // eslint-disable-next-line no-unused-vars
@@ -20,15 +21,56 @@ function handleFormPlaceholderPosition() {
             document.getElementsByClassName('formInput')[i].classList.add('labelRight');
     }
     console.log('sad');
-}
+};
+const fetchSitesData = async () => {
+    try {
+        const response = await fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50');
+        const json = await response.json();
+        console.log('parsed json', json)
+        return json;
+    } catch(ex) {
+        console.log('parsing failed', ex)
+
+    }
+};
+
+const createSiteList = (sites) => {
+    console.log(sites);
+    // eslint-disable-next-line no-restricted-syntax
+    for (const site of sites) {
+        const websiteIconContainer = document.createElement('div');
+        const websiteIconLink = document.createElement('a');
+        const websiteIcon = document.createElement('img');
+        const websiteName = document.createElement('div');
+
+        websiteIconContainer.classList.add('websiteIconContainer');
+        websiteIconLink.classList.add('websiteIconLink');
+        websiteIcon.classList.add('websiteIcon');
+        websiteName.classList.add('websiteName');
+
+        websiteName.innerHTML = site.appstoreName;
+
+        document.querySelector('.websiteList').appendChild(websiteIconContainer);
+        websiteIconContainer.appendChild(websiteIconLink);
+        websiteIconLink.appendChild(websiteIcon);
+        websiteIconContainer.appendChild(websiteName);
+    }
+};
+
 const init = async () => {
     try {
-        formRealignPlaceholder();
         await chayns.ready;
         chayns.ui.accordion.init();
+
     } catch (err) {
         console.error('No chayns environment found', err);
-    }
+    } 
+    
+    const sites = await fetchSitesData()
+    console.log(sites);
+    createSiteList(sites.Data);
+
+    formRealignPlaceholder();  
 };
 
 init();
