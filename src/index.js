191627e925cfbcf8059a1dc5ce3896987fee0068
Pinkernell <LPinkernell@tobit.ag>
2020-08-24 12:33:19 +0200

    add default website icon

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const formRealignPlaceholder = () => {
 };
 const fetchSitesData = async (skip, take) => {
     try {
-        const response = await fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=ahaus&Skip=${skip}&Take=${take}`);
+        const response = await fetch(`https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=pizza&Skip=${skip}&Take=${take}`);
         const json = await response.json();
         console.log('parsed json', json);
         return json;
@@ -24,27 +24,35 @@ const createSiteList = (sites) => {
     for (const site of sites) {
         const websiteIconContainer = document.createElement('div');
         const websiteIconLink = document.createElement('a');
-        const websiteIcon = document.createElement('img');
+
+        const websiteIcon = document.createElement('div');
+        // const websiteIconDef = document.createElement('div');
+
         const websiteName = document.createElement('div');
 
         websiteIconContainer.classList.add('websiteIconContainer');
         websiteIconLink.classList.add('websiteIconLink');
         websiteIcon.classList.add('websiteIcon');
+        // websiteIconDef.classList.add('websiteIconDef');
         websiteName.classList.add('websiteName');
+
+        // websiteIcon.classList.add('background');
+        // websiteIconDef.classList.add('background');
 
         websiteName.innerHTML = site.appstoreName;
         websiteIconLink.addEventListener('click', () => { chayns.openUrlInBrowser(`https://chayns.net/${site.siteId}`); });
-        websiteIcon.src = `https://sub60.tobit.com/l/${site.locationId}?size=65`;
-
+        websiteIcon.style = `background-image: url(https://sub60.tobit.com/l/${site.locationId}?size=65), url(https://sub60.tobit.com/l/152342?size=65)`;
 
         document.querySelector('.websiteList').appendChild(websiteIconContainer);
         websiteIconContainer.appendChild(websiteIconLink);
         websiteIconLink.appendChild(websiteIcon);
+        // websiteIconLink.appendChild(websiteIconDef);
         websiteIconContainer.appendChild(websiteName);
     }
 };
 const extendWebsiteList = async () => {
     const sites = await fetchSitesData(20 * fetchSitesCounter, 20);
+    const moreSites = await  fetchSitesData(20 * fetchSitesCounter + 1, 1);
     if (sites) {
         createSiteList(sites.Data);
         fetchSitesCounter += 1;
@@ -62,20 +70,23 @@ const sendFormInput = () => {
     // Seite
     const link = document.querySelector('.Link').value;
     const anmerkungen = document.querySelector('.Anmerkungen').value;
-    chayns.intercom.sendMessageToPage({
-        text: `${nachname} ${vorname} ${email} \n${stadt} ${plz} ${straße} \n${link} \n${anmerkungen} lllll`
-    }).then((data) => {
-        if (data.status === 200) {
-            document.querySelector('.Vorname').value = null;
-            document.querySelector('.Nachname').value = null;
-            document.querySelector('.PLZ').value = null;
-            document.querySelector('.Stadt').value = null;
-            document.querySelector('.Straße').value = null;
-            document.querySelector('.E-Mail').value = null;
-            document.querySelector('.Link').value = null;
-            document.querySelector('.Anmerkungen').value = null;
-        }
-    });
+
+    if (vorname && nachname && email && link) {
+        chayns.intercom.sendMessageToPage({
+            text: `${nachname} ${vorname} ${email} \n${stadt} ${plz} ${straße} \n${link} \n${anmerkungen} lllll`
+        }).then((data) => {
+            if (data.status === 200) {
+                document.querySelector('.Vorname').value = null;
+                document.querySelector('.Nachname').value = null;
+                document.querySelector('.PLZ').value = null;
+                document.querySelector('.Stadt').value = null;
+                document.querySelector('.Straße').value = null;
+                document.querySelector('.E-Mail').value = null;
+                document.querySelector('.Link').value = null;
+                document.querySelector('.Anmerkungen').value = null;
+            }
+        });
+    }
 };
 
 const init = async () => {
