8dcedebd4fca0f3c6930fe60e08faefd8dc06bd0
Pinkernell <LPinkernell@tobit.ag>
2020-08-26 13:12:30 +0200

    restructured code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-param-reassign */
 import './app.scss';
 import 'whatwg-fetch';
 
@@ -7,16 +5,22 @@ let fetchSitesCounter = 0;
 let searchFilter = 'Ahaus';
 let myVar;
 
-const formTestForInitialInput = () => {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const dynamicInput of document.querySelectorAll('.dynamicInput')) {
-        // eslint-disable-next-line no-restricted-syntax
-        formDynamicInput(dynamicInput);
-        dynamicInput.children[0].addEventListener('input', () => { formDynamicInput(dynamicInput); });
-        console.log(dynamicInput.children[0]);
+const addOtherEventListeners = () => {
+    document.querySelector('.extendButton').addEventListener('click', () => { extendWebsiteList(); });
+    document.querySelector('.formButton').addEventListener('click', () => { sendFormInput(); });
+    document.querySelector('.searchInput').addEventListener('input', () => { searchSetTimeout(); });
+};
+
+// Dynamic Input
+const dynamicInputAddEventListeners = () => {
+    const dynamicInputs = document.querySelectorAll('.dynamicInput');
+    for (let i = 0; i < dynamicInputs.length; i += 1) {
+        dynamicInputs[i].children[0].addEventListener('input', () => { inputDynamicInput(dynamicInputs[i]); });
+        // tests for initial input
+        inputDynamicInput(dynamicInputs[i]);
     }
 };
-const formDynamicInput = (dynamicInput) => {
+const inputDynamicInput = (dynamicInput) => {
     if (dynamicInput.children[0].value) {
         dynamicInput.classList.add('labelRight');
     } else {
@@ -24,6 +28,7 @@ const formDynamicInput = (dynamicInput) => {
     }
 };
 
+// Websites List
 const fetchSitesData = async (skip, take) => {
     chayns.showWaitCursor();
     try {
@@ -39,43 +44,33 @@ const fetchSitesData = async (skip, take) => {
     }
 };
 const createSiteList = async (sites) => {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const site of sites) {
+    for (let i = 0; i < sites.length; i += 1) {
         const websiteIconContainer = document.createElement('div');
         const websiteIconLink = document.createElement('a');
 
         const websiteIcon = document.createElement('div');
-        const websiteIconDef = document.createElement('div');
 
         const websiteName = document.createElement('div');
 
         websiteIconContainer.classList.add('websiteIconContainer');
         websiteIconLink.classList.add('websiteIconLink');
         websiteIcon.classList.add('websiteIcon');
-        websiteIconDef.classList.add('websiteIconDef');
         websiteName.classList.add('websiteName');
 
-        // websiteIcon.classList.add('background');
-        // websiteIconDef.classList.add('background');
-
-        websiteName.innerHTML = site.appstoreName;
-        websiteIconLink.addEventListener('click', () => { chayns.openUrlInBrowser(`https://chayns.net/${site.siteId}`); });
+        websiteName.innerHTML = sites[i].appstoreName;
+        websiteIconLink.addEventListener('click', () => { chayns.openUrlInBrowser(`https://chayns.net/${sites[i].siteId}`); });
 
         try {
-            // eslint-disable-next-line no-unused-vars
-            const response = await fetch(`https://sub60.tobit.com/l/${site.locationId}?size=65`);
-            websiteIcon.style = `background-image: url(https://sub60.tobit.com/l/${site.locationId}?size=65)`;
+            // eslint-disable-next-line no-await-in-loop
+            await fetch(`https://sub60.tobit.com/l/${sites[i].locationId}?size=65`);
+            websiteIcon.style = `background-image: url(https://sub60.tobit.com/l/${sites[i].locationId}?size=65)`;
         } catch {
             websiteIcon.style = 'background-image: url(https://sub60.tobit.com/l/152342?size=65)';
         }
 
-        // websiteIcon.style = `background-image: url(https://sub60.tobit.com/l/${site.locationId}?size=65)`;
-        // websiteIconDef.style = 'background-image: url(https://sub60.tobit.com/l/152342?size=65)';
-
         document.querySelector('.websiteList').appendChild(websiteIconContainer);
         websiteIconContainer.appendChild(websiteIconLink);
-        websiteIconLink.appendChild(websiteIconDef);
-        websiteIconDef.appendChild(websiteIcon);
+        websiteIconLink.appendChild(websiteIcon);
         websiteIconContainer.appendChild(websiteName);
     }
 };
@@ -98,12 +93,12 @@ const extendWebsiteList = async (list) => {
                 item.remove();
             }
         }
-
         createSiteList(sites.Data);
         fetchSitesCounter += 1;
     }
 };
 
+// Search Input
 const searchText = async () => {
     let newSearchFilter = document.querySelector('.searchInput').value;
 
@@ -113,7 +108,6 @@ const searchText = async () => {
     if (newSearchFilter !== searchFilter) {
         searchFilter = newSearchFilter;
         const list = await Promise.all(document.querySelector('.websiteList').children);
-        // eslint-disable-next-line no-restricted-syntax
         fetchSitesCounter = 0;
         extendWebsiteList(list);
     }
@@ -123,19 +117,61 @@ const searchSetTimeout = () => {
     myVar = setTimeout(() => { searchText(); }, 500);
 };
 
+// Form
+const formInitialFunction = () => {
+    // Autofill First & Last Name
+    if (chayns.env.user.isAuthenticated) {
+        document.querySelector('.Vorname').value = chayns.env.user.firstName;
+        document.querySelector('.Nachname').value = chayns.env.user.lastName;
+    }
+    //
+    const inputs = document.querySelectorAll('.mandatory');
+    for (let i = 0; i < inputs.length; i += 1) {
+        formTestForInput(inputs[i]);
+    }
+};
+const formAddEventListeners = () => {
+    const inputs = document.querySelectorAll('.mandatory');
+
+    for (let i = 0; i < inputs.length; i += 1) {
+        inputs[i].children[0].addEventListener('input', () => { formTestForInput(inputs[i]); });
+    }
+};
+const formTestForInput = (input) => {
+    // Tests an Input Element for Input
+    if (!input.children[0].value) {
+        input.children[1].classList.add('formInputMissing');
+    } else if (input.children[0].value) {
+        input.children[1].classList.remove('formInputMissing');
+    }
+    // Sets Transparency of button
+    setTransparencyOfButton();
+};
+const setTransparencyOfButton = () => {
+    const vorname = document.querySelector('.Vorname').value;
+    const nachname = document.querySelector('.Nachname').value;
+    const email = document.querySelector('.E-Mail').value;
+    const link = document.querySelector('.Link').value;
+    const button = document.querySelector('.formButton');
+    if (vorname && nachname && email && link && button.classList.contains('grey')) {
+        button.classList.remove('grey');
+    } else if (!vorname || !nachname || !email || !link) {
+        button.classList.add('grey');
+    }
+};
 const sendFormInput = () => {
     if (chayns.env.user.isAuthenticated) {
-        // Name
+        // saves Inputs
         const vorname = document.querySelector('.Vorname').value;
         const nachname = document.querySelector('.Nachname').value;
-        // Adresse
         const plz = document.querySelector('.PLZ').value;
         const stadt = document.querySelector('.Stadt').value;
         const straße = document.querySelector('.Straße').value;
         const email = document.querySelector('.E-Mail').value;
-        // Seite
         const link = document.querySelector('.Link').value;
         const anmerkungen = document.querySelector('.Anmerkungen').value;
+
+        // tests for mandatory inputs
         if (vorname && nachname && email && link) {
             chayns.intercom.sendMessageToPage({
                 text: `Name: ${vorname} ${nachname} \nEmail: ${email} \nAdresse: ${straße} ${plz} ${stadt} \nSeite: ${link} \n${anmerkungen}`
@@ -143,6 +179,7 @@ const sendFormInput = () => {
                 if (data.status === 200) {
                     chayns.dialog.alert('', 'Wir haben Deine Anfrage erhalten.').then(console.log);
 
+                    // resets all inputs except for first and last name
                     document.querySelector('.PLZ').value = null;
                     document.querySelector('.Stadt').value = null;
                     document.querySelector('.Straße').value = null;
@@ -150,66 +187,27 @@ const sendFormInput = () => {
                     document.querySelector('.Link').value = null;
                     document.querySelector('.Anmerkungen').value = null;
                 }
-                formDynamicInput(document.querySelector('.PLZ').parentElement);
-                formDynamicInput(document.querySelector('.Stadt').parentElement);
-                formDynamicInput(document.querySelector('.Straße').parentElement);
+                inputDynamicInput(document.querySelector('.PLZ').parentElement);
+                inputDynamicInput(document.querySelector('.Stadt').parentElement);
+                inputDynamicInput(document.querySelector('.Straße').parentElement);
 
-                formTestForInput(document.querySelector('.E-Mail').parentElement);
-                formDynamicInput(document.querySelector('.E-Mail').parentElement);
-                formTestForInput(document.querySelector('.Link').parentElement);
-                formDynamicInput(document.querySelector('.Link').parentElement);
+                inputDynamicInput(document.querySelector('.E-Mail').parentElement);
+                inputDynamicInput(document.querySelector('.Link').parentElement);
+
+                document.querySelector('.Link').parentElement.children[1].classList.add('formInputMissing');
+                document.querySelector('.E-Mail').parentElement.children[1].classList.add('formInputMissing');
             });
         }
     } else {
-        login();
+        loginAndSend();
     }
 };
-const formAddEventListeners = () => {
-    const inputs = document.querySelectorAll('.mandatory');
-
-    // eslint-disable-next-line no-restricted-syntax
-    for (const input of inputs) {
-        input.children[0].addEventListener('input', () => { formTestForInput(input); });
-    }
-};
-const formTestForInput = (input) => {
-    if (!input.children[0].value) {
-        input.children[1].classList.add('formInputMissing');
-    } else if (input.children[0].value) {
-        input.children[1].classList.remove('formInputMissing');
-    }
-    // Sets Transparency of button
-    const vorname = document.querySelector('.Vorname').value;
-    const nachname = document.querySelector('.Nachname').value;
-    const email = document.querySelector('.E-Mail').value;
-    const link = document.querySelector('.Link').value;
-    const button = document.querySelector('.formButton');
-    if (vorname && nachname && email && link) {
-        button.classList.remove('grey');
-    } else if (!vorname || !nachname || !email || !link) {
-        button.classList.add('grey');
-    }
-};
-const formInitialFunction = () => {
-    if (chayns.env.user.isAuthenticated) {
-        document.querySelector('.Vorname').value = chayns.env.user.firstName;
-        document.querySelector('.Nachname').value = chayns.env.user.lastName;
-    }
-
-
-    const inputs2 = document.querySelectorAll('.mandatory');
-    // eslint-disable-next-line no-restricted-syntax
-    for (const input of inputs2) {
-        formTestForInput(input);
-    }
-};
-function login() {
+function loginAndSend() {
     // optional -> prevents site reload
     chayns.addAccessTokenChangeListener(() => {
         console.log('login successful');
         sendFormInput();
     });
-
     // no reload tapp after login
     chayns.login();
 }
@@ -221,18 +219,16 @@ const init = async () => {
     } catch (err) {
         console.error('No chayns environment found', err);
     }
-    document.querySelector('.extendButton').addEventListener('click', () => { extendWebsiteList(); });
-    document.querySelector('.formButton').addEventListener('click', () => { sendFormInput(); });
 
-    document.querySelector('.searchInput').addEventListener('input', () => { searchSetTimeout(); });
+    addOtherEventListeners();
     // Initial Form Input Test
     formInitialFunction();
-
+    // Adds event Listeners to Mandatory Input Fields
     formAddEventListeners();
 
     extendWebsiteList();
 
-    formTestForInitialInput();
+    dynamicInputAddEventListeners();
 };
 
 init();
